55bd1cf739d3718fdd89702b5454da32f5a14841
John Louie Navales <104504427+johnlouie09@users.noreply.github.com>
2024-04-01 15:11:46 +0800

    Add results page.

--- app/index.php
+++ app/index.php
@@ -64,6 +64,7 @@ $routes = [
     [ "name" => "User Assignments", "route" => "crud/assignment"       ],
     [ "name" => "Eliminations"    , "route" => "crud/eliminations"     ],
     [ "name" => "Arrangements"    , "route" => "crud/orders"           ],
+    [ "name" => "Top 5 Winners"   , "route" => "results"               ],
 ];
 ?>
 <!DOCTYPE html>
